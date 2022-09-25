42f02c34bddf5a9b6b6ffb56d4ae3ca471b54037
bouaichiDev <bouaichibadr@gmail.com>
2022-09-25 16:09:11 +0100

    try to see some msg

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const http=require('http');
+const path =require('path');
+const fs =require('fs');
+const fsPromises=require('fs').promises;
+
+
+
+const server =http.createServer((req,res)=>{
+    console.log(req.url,req.method);
+    
+})
+const PORT =process.env.PORT||3500;
+server.listen(PORT,()=>console.log(`Server is runing in ${PORT}`));
+
 // const p= new Promise((result,error)=>{
 //     const statut =false;
 //         if (statut) {
@@ -9,27 +23,27 @@
 
 // p.then((user)=>{console.log(user)}).catch((err)=>{console.log(err.message)})
 
-function getRepot(id) {
-    return new Promise((result,error)=>{
-        setTimeout(() => {
-            console.log('Repot get .....');
-            result({'userID':id,'name':'Repot badr'});
-        }, 2000);
-    })
-}
-function getUser(id) {
-    return new Promise((result,error)=>{
-        setTimeout(() => {
-            console.log('User get .....');
-            result({'id':id,'name':'badr'});
-        }, 2000 );
-    })
-}
-console.log('BEGIN');
-getUser(22).then((user)=>{
-    console.log("Here we go user",user);
-    return getRepot(user.id);
-}).then((repot)=>{
-    console.log("Here we go repot",repot);
-});
-console.log('END');
+// function getRepot(id) {
+//     return new Promise((result,error)=>{
+//         setTimeout(() => {
+//             console.log('Repot get .....');
+//             result({'userID':id,'name':'Repot badr'});
+//         }, 2000);
+//     })
+// }
+// function getUser(id) {
+//     return new Promise((result,error)=>{
+//         setTimeout(() => {
+//             console.log('User get .....');
+//             result({'id':id,'name':'badr'});
+//         }, 2000 );
+//     })
+// }
+// console.log('BEGIN');
+// getUser(22).then((user)=>{
+//     console.log("Here we go user",user);
+//     return getRepot(user.id);
+// }).then((repot)=>{
+//     console.log("Here we go repot",repot);
+// });
+// console.log('END');
